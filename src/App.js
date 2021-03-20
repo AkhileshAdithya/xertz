8b2f403d43631f6fdef88a52d61eaad933b2c34d
AkhileshAdithya <obliviongod16@gmail.com>
2021-03-21 04:33:23 +0530

    Change to hashrouter to fix react routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from './components/Header';
 import Body from './components/Body'
 
 export default class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <div>
                     <Header />
                     <Body />
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
