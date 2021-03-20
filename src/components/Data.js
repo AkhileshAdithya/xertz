04d4ec347b6cf4a45e99dbae0ab8c001069de901
Akhilesh Adithya <38578610+AkhileshAdithya@users.noreply.github.com>
2021-03-21 04:17:07 +0530

    Update Data.js
    test

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,6 @@
 var data = [
     {   name:"Reverend Insanity",
-        pic:"Resources/ReverendInsanity/reverendInsanity.png" ,
+        pic:"/Resources/ReverendInsanity/reverendInsanity.png" ,
         author:"Gu Zhu Ren Zhu",
         synopsis:"A story of a villain, Fang Yuan who was reborn 500 years into the past with the Spring Autumn Cicada he painstakingly refined. With his profound wisdom, battle and life experiences, he seeks to overcome his foes with skill and wit! Ruthless and amoral, he has no need to hold back as he pursues his ultimate goals. In a world of cruelty where one cultivates using Gu – [magical creatures of the world] – Fang Yuan must rise up above all with his own power.",
         slug:"reverend-insanity",
@@ -14,7 +14,7 @@ var data = [
         webnovel: true
     },
     {   name:"The Fifth Science",
-        pic:"Resources/FifthScience/fifthScience.jpg" ,
+        pic:"/Resources/FifthScience/fifthScience.jpg" ,
         author:"exurb1a",
         synopsis:"The Galactic Human Empire was built atop four sciences: logic, physics, psychology, and sociology. Standing on those pillars, humans spent 100,000 years spreading out into the galaxy: warring, exploring, partying — the usual. Then there was the fifth science. And that killed the empire stone dead. The Fifth Science is a collection of 12 stories, beginning at the start of the Galactic Human Empire and following right through to its final days. We’ll see some untypical things along the way, meet some untypical folk: galactic lighthouses from the distant future, alien tombs from the distant past, murderers, emperors, archaeologists and drunks; mad mathematicians attempting to wake the universe itself up.",
         slug:"fifth-science",
